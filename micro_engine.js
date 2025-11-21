3312665f10c9ad67b2d768645cacdee95df886f9
aaronwolk00 <aaronwolk00@gmail.com>
2025-11-21 01:20:42 -0500

    Add files via upload

--- micro_engine.js
+++ micro_engine.js
@@ -541,7 +541,7 @@ function clamp(x, lo, hi) {
    */
   export function samplePassOutcome(params, rng) {
     const {
-      qbAccuracyRating = 60,
+      qbAccuracyRating = 65,
       qbProcessingRating = 60,
       qbUnderPressureRating = 60,
       olPassBlockRating = 60,
